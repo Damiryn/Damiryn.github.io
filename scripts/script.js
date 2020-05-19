70b4a010a6192d775b5d460f6ec9d369820738c1
Damiryn <salam0512@mail.ru>
2020-05-19 20:39:05 +0300

    lesson 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
- console.log ("Hello")
+/* console.log ("Hello")
 document.write("Bye")
 
 let func = function(str, name) {
@@ -22,7 +22,9 @@ function result (){
 	else {
 		console.log ("Miss");
 	}
-} 
+} */
+
+
 let def = 0;
 let isDec = false;
 
@@ -63,9 +65,120 @@ function check (num) {
 
 
 let item = 0;
-
-while (item < 5) {
+	
+while (item < 10) {
 	$ ('#itemlist').append(`<li>${item}</li>`);
 	console.log (item);
 	item++;
 }
+
+
+
+function list (){
+	let text = prompt('Vvedi text');
+
+	console.log(text);
+
+	let html = `
+		<li>
+			<div class="itemlist">
+				<span class="target" onmouseenter="toggleColor(this, true)" onmouseleave="toggleColor(this)">${text}</span>
+				<button class="dilete" onclick="dellist(this.parentNode.parentNode)">x</button>
+			</div>
+		</li>`;
+
+	$('#itemlist').append(html);
+}
+
+
+function addHeader (){
+	let textHader = prompt('Название заголовка?');
+	let header = `
+			<a>
+			<span class="addMenu">${textHader}</span>
+			<button class="dilete" onclick="dellist(this.parentNode)">Удалить</button>
+			</a>`;
+
+	$('.menu').append(header);
+}
+
+function dellist (element){
+	console.log('Удалить');
+	console.log(element);
+
+	$(element).replaceWith();
+}
+
+function toggleColor (element, active = false){
+	console.log (element, active);
+
+	let activeColor = 'green';
+	let defalutColor = 'blue';
+
+	if (active){
+		$(element).css('color', activeColor);
+	} else {
+		$(element).css('color', defalutColor);
+	}
+}
+
+
+
+
+/*
+function HeaderColorc ()
+
+var z = 0;
+var x = 5;
+
+while (z < 10) {
+    $ ('#itemlist').append(`<li>${z + x}</li>`);
+    console.log (z);
+    z++;
+}; 
+
+//
+
+var z = 0;
+var x = 'text';
+
+while (z < 10) {
+    $ ('#itemlist').append(`<li>${z + x}</li>`);
+    console.log (z);
+    z++;
+};
+
+
+
+let custom = 0;
+function ret(a){
+	return a * 2;
+}
+
+custom = ret(10);
+console.log (custom);
+*/
+
+// Zadacha
+
+const rate_tax=0.1;
+const rate_tel=100;
+const rate_acc=10;
+const bank_balans=300;
+
+let stel= 0;
+
+function SummaTell() {
+
+	stel = rate_tel + rate_acc + (rate_tel * rate_tax);
+	console.log (stel);
+	return stel;
+	
+};
+/*
+
+for (stel = 0; stel < bank_balans; stel = stel + stel) {
+			if (stel < bank_balans)
+			console.log ('Yes');
+			break;
+		};*/
