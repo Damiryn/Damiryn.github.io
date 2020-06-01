89ab5be8427d61074e5e0f459bea521115e2243c
Damiryn <salam0512@mail.ru>
2020-06-01 19:44:14 +0300

    makeup

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,67 @@
-/* console.log ("Hello")
-document.write("Bye")
+//Добавляем хедер из масива
+let menuLinks = [
+	'Home',
+	'Media',
+	'Contancs',
+	'Follow',
+];
 
-let func = function(str, name) {
-return str + "_" + name
-}
-function calc (a){
-	return a*2;
-}
+function generateMenuLinks (){
+	let html = '';
 
-if (calc (2)>10) {
-	console.log ("Right");
-}
-else {
-	console.log ("Miss");
-}
-function result (){
-	let x = prompt ("Number?!");
-	if (calc (x)>10) {
-		console.log ("Ne smotrel krepkogo oreha");
+	for (const items of menuLinks){
+		html += `<a href="">${items}</a>`;
 	}
-	else {
-		console.log ("Miss");
-	}
-} */
+ 	return html;
+
+}
+
+//Добавить нижний хэдер
+function addHeader (){
+	let textHader = prompt('Название заголовка?');
+	let header = `
+			<span>
+			<a class="addMenu">${textHader}</a>
+			<button class="dilete" onclick="dellist(this.parentNode)">Удалить</button>
+			</span>`;
+
+	$('.downmenu').append(header);
+}
+
+//Поменять цвета текста
+function Selectors(){
+
+	$('.items1 li:first').css('color', 'blue');
+	$('.items1 li:last').css('color', 'blue');
+	$('i+b').css('color', 'blue');
+	$('[value="Добавить задачу"]').css('background-color','pink');
+};
+
+function SelOff(){
+
+	$('.items1 li:first').css('color', 'black');
+	$('.items1 li:last').css('color', 'black');
+	$('i+b').css('color', 'red');
+	$('[value="Добавить задачу"]').css('background-color','#add9fe');
+};
+
+//Кнопка Добавить задачу
+function addTasks (){
+	let textTasks = $('#addTasks').val();
+	let task = `
+			<div class="addTasks">
+				<div>${textTasks}
+				<button class="dilete" onclick="dellist(this.parentNode.parentNode)">x</button>
+				</div>
+			</div>`;
+
+	$('#Tasks').append(task);
+}
 
 
+//Функции к кнопкам
+
+//увеличить, уменьшить, проверка
 let def = 0;
 let isDec = false;
 
@@ -63,7 +100,7 @@ function check (num) {
 	}
 } 
 
-
+//кнопка AddList
 let item = 0;
 	
 while (item < 10) {
@@ -71,8 +108,6 @@ while (item < 10) {
 	console.log (item);
 	item++;
 }
-
-
 
 function list (){
 	let text = prompt('Vvedi text');
@@ -88,30 +123,6 @@ function list (){
 		</li>`;
 
 	$('#itemlist').append(html);
-}
-
-
-function addHeader (){
-	let textHader = prompt('Название заголовка?');
-	let header = `
-			<span>
-			<a class="addMenu">${textHader}</a>
-			<button class="dilete" onclick="dellist(this.parentNode)">Удалить</button>
-			</span>`;
-
-	$('.downmenu').append(header);
-}
-
-function addTasks (){
-	let textTasks = $('#addTasks').val();
-	let task = `
-			<div class="addTasks">
-				<div>${textTasks}
-				<button class="dilete" onclick="dellist(this.parentNode.parentNode)">x</button>
-				</div>
-			</div>`;
-
-	$('#Tasks').append(task);
 }
 
 function dellist (element){
@@ -134,46 +145,70 @@ function toggleColor (element, active = false){
 	}
 }
 
+//Кнопка Записать новое значение
+let user = '';
 
+function setValue(){
+	let value = prompt('Who are you?');
 
-
-/*
-function HeaderColorc ()
-
-var z = 0;
-var x = 5;
-
-while (z < 10) {
-    $ ('#itemlist').append(`<li>${z + x}</li>`);
-    console.log (z);
-    z++;
-}; 
-
-//
-
-var z = 0;
-var x = 'text';
-
-while (z < 10) {
-    $ ('#itemlist').append(`<li>${z + x}</li>`);
-    console.log (z);
-    z++;
-};
-
-
-
-let custom = 0;
-function ret(a){
-	return a * 2;
+	$('#Value').html(value);
 }
 
-custom = ret(10);
-console.log (custom);
-*/
+//Кнопка скрыть/показать
+function setHide(){
+	let hideElement = $('#General');
+	let button = $('#HideButton');
+	
+		if($(button).hasClass('active')){ // if всегда работает тогда когда true...
+			$(button).removeClass('active');
+			$(button).text('Скрыть');
+			$(button).css('color', 'black');
+			$(hideElement).show();
+		} else {						// ....иначе будет выполнятс else
+			$(button).addClass('active');
+			$(button).text('Показать');
+			$(button).css('color', 'red');
+			$(hideElement).hide();
+		}
 
-// Zadacha
+	console.log($(button).hasClass('active'));
+	console.log(button);
+}
 
+// Popup window Кнопка Показать окно
+let modalCounter = 0;
 
+function createPopup(){
+	console.log('Popup Window');
+	
+	let container = document.createElement('div');
+	let textModal = prompt('Дай текст')
+	
+	let id = 'modal_' + modalCounter;
+
+	container.classList.add('modal');
+	container.setAttribute('id', id) // id="modal_0"
+	container.innerHTML = `
+		<div class="modal-content">
+   		 	<span class="close">&times;</span>
+    		<p>${textModal}</p>								
+  		</div>`;
+	
+	console.log(container);
+
+	modalCounter++;
+
+	$('#modals').html(container);
+	container.style.display = 'flex';
+
+	let closeButton = $('#' + id).children().children('span');
+	
+	$(closeButton).on('click', function(){						//удаляем окно
+		document.getElementById(id).remove(); 			
+	});
+}
+
+// Zadacha, калькулятор телефонов
 function SummaTell() {
 
 const rate_tax = 0.1;
@@ -215,87 +250,39 @@ let SumAcc = 0;
 
 };
 
-//function delRezultTelepphone{ 	$('#Telephone').empty(); };
+//Переключатель картинок ??
 
-	//$('#Telephone').html(SummaTell ());
+function addImage(){
 
-
-
-let menuLinks = [
-	'Home',
-	'Media',
-	'Contancs',
-	'Follow',
-];
-
-function generateMenuLinks (){
-	let html = '';
-
-	for (const items of menuLinks){
-		html += `<a href="#">${items}</a>`;
-	}
- 	return html;
-
-}
-
-let user = '';
-
-function setValue(){
-	let value = prompt('Who are you?');
-
-	$('#Value').html(value);
-}
-
-function setHide(){
-	let hideElement = $('#General');
-	let button = $('#HideButton');
+	let clickImg = $('.changeImg').clone().append();
+			
+	console.log('Hello')
 	
-		if($(button).hasClass('active')){ // if всегда работает тогда когда true...
-			$(button).removeClass('active');
-			$(button).text('Скрыть');
-			$(button).css('color', 'black');
-			$(hideElement).show();
-		} else {						// ....иначе будет выполнятс else
-			$(button).addClass('active');
-			$(button).text('Показать');
-			$(button).css('color', 'red');
-			$(hideElement).hide();
-		}
+	$('#imagecontainer').append(clickImg);
 
-	console.log($(button).hasClass('active'));
-	console.log(button);
 }
+// onmouseenter="toggleImage(this, true)" onmouseleave="toggleImage(this)"
+// function toggleImage(el, act){
 
+// 	let = ;
+// 	let = ;
 
-function Selectors(){
+// 	if (act){
+// 		$(el).css('color', activeColor);
+// 	} else {
+// 		$(el).css('color', defalutColor);
+// 	}
 
-	$('.items1 li:first').css('color', 'yellow');
-	$('.items1 li:last').css('color', 'yellow');
-	$('i+b').css('color', 'yellow');
-	$('[value="Добавить"]').css('background-color','pink');
-
-
-};
-
-
-function SelOff(){
-
-	$('.items1 li:first').css('color', 'blue');
-	$('.items1 li:last').css('color', 'blue');
-	$('i+b').css('color', 'red');
-	$('[value="Добавить"]').css('background-color','#00ffff');
-};
-
-
+// }
 
 
 //функция "запуска страницы", всегда должна быть в конце!
 $(function(){
 	
-	$('#menuLinks').append(generateMenuLinks ());
+	$('#menuLinks').append(generateMenuLinks ()); // Добавляет массив Create header в конец header
 
 	if (!localStorage.getItem('name')){
-		localStorage.setItem ('name', 'Damir');
+		localStorage.setItem ('name', 'Damir');	//Делает Damir под заголовком (кеш браузера)
 	} else {
 		user= localStorage.getItem('name');
 	}
@@ -307,8 +294,10 @@ $(function(){
 			console.log(!!attr); 			// !! как '-' на '-'
 		
 		if (!!attr){
-			if (attr === 'HideButton'){
+			if (attr === 'HideButton'){		//кнопка скрыть\показать
 				setHide();
+			}else if(attr === 'createPopup'){
+					createPopup();
 			}else{
 				console.log('No ID');
 			}
@@ -320,14 +309,94 @@ $(function(){
 				//case "Показать":  				// if (condition)
 				case "AddList":  					// if (condition)
 					list ();
+					break;
+				case "Записать новое значение":
+					setValue();
+					break;
 				case "Поменять цвета":
 					Selectors();
 					break;							//без брейка работает только 1 из 2 кнопок (либо-либо) потому что проскакивает?
 				case "Вернуть как было": 		
 					SelOff();
+					break;							//без брейка может "сломаться" кнопка и выполнять то что идет следующим
+				case "Добавить":
+					addHeader ();
 			default: 								// else
 				console.log(text);
 			}
 		}
 	});
 });
+
+
+
+
+
+//Мусор/черновики
+
+//function delRezultTelepphone{ 	$('#Telephone').empty(); };
+
+	//$('#Telephone').html(SummaTell ());
+
+//Переменную можно прокинуть в строку двумя способами `string ${переменная} string` || 'string'+переменная+'string'
+
+
+/* console.log ("Hello")
+document.write("Bye")
+
+let func = function(str, name) {
+return str + "_" + name
+}
+function calc (a){
+	return a*2;
+}
+
+if (calc (2)>10) {
+	console.log ("Right");
+}
+else {
+	console.log ("Miss");
+}
+function result (){
+	let x = prompt ("Number?!");
+	if (calc (x)>10) {
+		console.log ("Ne smotrel krepkogo oreha");
+	}
+	else {
+		console.log ("Miss");
+	}
+} */
+
+/*
+function HeaderColorc ()
+
+var z = 0;
+var x = 5;
+
+while (z < 10) {
+    $ ('#itemlist').append(`<li>${z + x}</li>`);
+    console.log (z);
+    z++;
+}; 
+
+//
+
+var z = 0;
+var x = 'text';
+
+while (z < 10) {
+    $ ('#itemlist').append(`<li>${z + x}</li>`);
+    console.log (z);
+    z++;
+};
+
+
+
+let custom = 0;
+function ret(a){
+	return a * 2;
+}
+
+custom = ret(10);
+console.log (custom);
+*/
